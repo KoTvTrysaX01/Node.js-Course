051b4343236daebb9639c1306dce6855c8d6641d
Vadim Elshin <elshinvadim@gmail.com>
2025-08-06 22:01:56 +0200

    NPM Commands & Installed a few packages

---
+++ 12-NPM.js
@@ -0,0 +1,10 @@
+// npm - Global command that comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+
+const _ = require('lodash');
